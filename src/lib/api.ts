03827c4353d07761ebaa899684a56e31baf313f0
aqumu <aqumu.vm@gmail.com>
2025-11-09 22:40:18 +0700

    set api url to a fixed one

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 import { supabase } from "./supabaseClient.ts";
 
 const api = axios.create({
-  baseURL: "https://hq3jpig5r6b6.share.zrok.io/api",
+  baseURL: "https://api.accessibility-analyzer.ru",
   timeout: 5000,
   withCredentials: true,
 });
@@ -16,11 +16,6 @@ api.interceptors.request.use(async (config) => {
 
   if (token && config.headers) {
     config.headers["Authorization"] = `Bearer ${token}`;
-  }
-
-  // Add Zrok bypass header
-  if (config.headers) {
-    config.headers["skip_zrok_interstitial"] = "1";
   }
 
   return config;
