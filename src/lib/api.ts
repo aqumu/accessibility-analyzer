b0157025067d0431e764f62984579409e326ca56
aqumu <aqumu.vm@gmail.com>
2025-11-16 12:25:03 +0700

    fix loading

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 import { supabase } from "./supabaseClient.ts";
 
 const api = axios.create({
-  baseURL: "https://localhost:8000/",
+  baseURL: "http://localhost:8000/api/",
   timeout: 10000,
   withCredentials: true,
 });
